0213ae9cfddd9d97c6b24958c6e6d2fd6f64507c
Julian M. Kunkel <juliankunkel@googlemail.com>
2018-11-18 17:25:55 +0000

    simpler echo

--- io-500-gen.sh
+++ io-500-gen.sh
@@ -38,43 +38,43 @@ echo "echo [IOR EASY WRITE]"
 echo "mkdir -p $DATA_DIR/ior_easy $DATA_DIR/ior_easy $DATA_DIR/ior_hard $DATA_DIR/mdt_hard $DATA_DIR/mdt_easy $DATA_DIR/pfind_results"
 echo "# Please add here additional scripts to setup/prepare the directories like lfs setstripe"
 echo $MPIRUN $BIN/ior -w -C -Q 1 -g -G 27 -k -e $IOR_EASY_ARGS -o $DATA_DIR/ior_easy/ior_file_easy -O stoneWallingStatusFile=$DATA_DIR/ior_easy/stonewall -O stoneWallingWearOut=1 -D $STONEWALL_TIMER
-
+echo
 echo "echo [MDTEST EASY WRITE]"
 echo $MPIRUN $BIN/mdtest -C -F -d $DATA_DIR/mdt_easy $MDTEST_EASY $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_easy-stonewall -W $STONEWALL_TIMER
-
+echo
 echo "touch $DATA_DIR/timestampfile"
-
+echo
 echo "echo [IOR HARD WRITE]"
 echo $MPIRUN $BIN/ior -w -C -Q 1 -g -G 27 -k -e -t 47008 -b 47008 -s $IOR_HARD_IO_COUNT $IOR_HARD_EXTRA_ARGS  -o $DATA_DIR/ior_hard/IOR_file -O stoneWallingStatusFile=$DATA_DIR/ior_hard/stonewall -O stoneWallingWearOut=1 -D $STONEWALL_TIMER
-
+echo
 echo "echo [MDTEST HARD WRITE]"
 echo $MPIRUN $BIN/mdtest -C -t -F -w 3901 -e 3901 -d $DATA_DIR/mdt_hard -n $MDTEST_HARD_FILE_COUNT $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_hard-stonewall  -W $STONEWALL_TIMER
-
+echo
 echo "echo [PFIND EASY]"
 echo "# You may change the pfind command!"
 echo $MPIRUN $BIN/pfind $DATA_DIR -newer $DATA_DIR/timestampfile -size 3901c -name *01* -s $STONEWALL_TIMER -r $DATA_DIR/pfind_results
-
+echo
 echo "echo [IOR EASY READ]"
 echo $MPIRUN $BIN/ior -r -R -C -Q 1 -g -G 27 -k -e $IOR_EASY_ARGS -o $DATA_DIR/ior_easy/ior_file_easy -O stoneWallingStatusFile=$DATA_DIR/ior_easy/stonewall
-
+echo
 echo "echo [MDTEST EASY STAT]"
 echo $MPIRUN $BIN/mdtest -T -F -d $DATA_DIR/mdt_easy $MDTEST_EASY $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_easy-stonewall
-
+echo
 echo "echo [IOR HARD READ]"
 echo $MPIRUN $BIN/ior -r -R -C -Q 1 -g -G 27 -k -e -t 47008 -b 47008 -s $IOR_HARD_IO_COUNT $IOR_HARD_EXTRA_ARGS  -o $DATA_DIR/ior_hard/IOR_file -O stoneWallingStatusFile=$DATA_DIR/ior_hard/stonewall
-
+echo
 echo "echo [MDTEST HARD STAT]"
 echo $MPIRUN $BIN/mdtest -T -t -F -w 3901 -e 3901 -d $DATA_DIR/mdt_hard -n $MDTEST_HARD_FILE_COUNT $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_hard-stonewall
-
+echo
 echo "echo [MDTEST EASY DELETE]"
 echo $MPIRUN $BIN/mdtest -r -F -d $DATA_DIR/mdt_easy $MDTEST_EASY $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_easy-stonewall
-
+echo
 echo "echo [MDTEST HARD READ]"
 echo $MPIRUN $BIN/mdtest -E -t -F -w 3901 -e 3901 -d $DATA_DIR/mdt_hard -n $MDTEST_HARD_FILE_COUNT $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_hard-stonewall
-
+echo
 echo "echo [MDTEST HARD DELETE]"
 echo $MPIRUN $BIN/mdtest -r -t -F -w 3901 -e 3901 -d $DATA_DIR/mdt_hard -n $MDTEST_HARD_FILE_COUNT $MDTEST_HARD_EXTRA_ARGS -x $DATA_DIR/mdt_hard-stonewall
-
+echo
 echo "echo [IO-500 COMPLETED] Now use io-500-score.sh to compute the score!"
 )  > $OUT_SCRIPT_FILE
 
